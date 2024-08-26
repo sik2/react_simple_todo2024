5d20f22667a4698d8dce9c81b81217b84e1d88d0
sik2 <bickwnag123@gmail.com>
2024-08-26 20:26:45 +0900

    delete, add, upadate

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -14,21 +14,48 @@ function Todos() {
     }, [])
 
     const onInsert = (todo) => {
-        const newTodos = [...todos, { id: lastId, todo, completed: false }]
-        setTodos(newTodos)
-        lastId++
+        // const newTodos = [...todos, { id: lastId, todo, completed: false }]
+        // setTodos(newTodos)
+        // lastId++
+        fetch('https://dummyjson.com/todos/add', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                todo,
+                completed: false,
+                userId: 5,
+            }),
+        })
+            .then((res) => res.json())
+            .then(console.log)
     }
 
     const onDelete = (id) => {
-        const newTodos = todos.filter((todo) => todo.id != id)
-        setTodos(newTodos)
+        // const newTodos = todos.filter((todo) => todo.id != id)
+        // setTodos(newTodos)
+
+        fetch(`https://dummyjson.com/todos/${id}`, {
+            method: 'DELETE',
+        })
+            .then((res) => res.json())
+            .then(console.log)
     }
 
     const onToggle = (id) => {
-        const newTodos = todos.map((todo) =>
-            todo.id === id ? { ...todo, completed: !todo.completed } : todo
-        )
-        setTodos(newTodos)
+        // const newTodos = todos.map((todo) =>
+        //     todo.id === id ? { ...todo, completed: !todo.completed } : todo
+        // )
+        // setTodos(newTodos)
+
+        fetch(`https://dummyjson.com/todos/${id}`, {
+            method: 'PUT' /* or PATCH */,
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                completed: false,
+            }),
+        })
+            .then((res) => res.json())
+            .then(console.log)
     }
 
     return (
